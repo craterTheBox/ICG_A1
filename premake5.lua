76d2e1061b9ae13a9296b1573ec3b9b600a2593f
Carter <carter.menary@gmail.com>
2020-02-04 02:07:39 -0500

    [Add] premake
    please please please

---
+++ premake5.lua
@@ -0,0 +1,77 @@
+workspace ("ICG_A1")
+    location("./")
+
+    defines "_CRT_SECURE_NO_WARNINGS"
+    configurations { "Debug", "Release" }
+	platforms "x64"
+	
+	includedirs {
+		"./Engine",
+		"./Engine/Externals/fmod/include",
+		"./Engine/Externals/freetype/include",
+		"./Engine/Externals/glad/include",
+		"./Engine/Externals/glfw3/include",
+		"./Engine/Externals/glm/include",
+		"./Engine/Externals/imgui/include",
+        "./Engine/Externals/stb/include",
+        "./include"
+	}
+
+	filter "platforms:x64"
+		system "Windows"
+		architecture "x64"
+		defines "Win64"
+    
+    libdirs {
+		"libs/%{cfg.buildcfg}",
+    }
+    
+    filter "platforms:x64"
+		system "windows"
+		architecture "x64"
+		defines "Win64"
+	
+	filter "system:windows"
+		systemversion "latest"
+		
+		defines {
+			"GLFW_INCLUDE_NONE",
+			"WINDOWS"
+		}
+		
+	filter "configurations:Debug"
+		defines "DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "NDEBUG"
+		runtime "Release"
+        optimize "on"
+		
+		filter{}
+
+ project ("Assignment 1")
+	kind "ConsoleApp"
+	configuration "windows"
+	targetname ("Assignment 1")
+
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	
+	targetdir ("./Build/bin/%{cfg.platform}/%{cfg.buildcfg}")
+    objdir    ("./Build/obj/%{cfg.platform}/%{cfg.buildcfg}")
+
+	
+	links {
+		"capp",
+		"Externals"
+	}
+	
+	files {
+		"./include/**.h",
+		"./include/**.hpp",
+		"./src/**.cpp",
+		"./Assets/**"
+	}
